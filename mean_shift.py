374a2789e115f165ffb73a82de3b2681a68b1c43
Manohar Mukku <manoharmukku@gmail.com>
2019-03-21 16:08:27 +0530

    Initial unfixed code

--- mean_shift.py
+++ mean_shift.py
@@ -4,11 +4,35 @@ Date: 21 Mar 2019
 Desc: Mean shift clustering algorithm
 """
 
+import numpy as np
+
 class MeanShift:
-    def __init__(self, radius=4):
+    def __init__(self, radius=1):
         self.radius = radius
 
     def fit(X):
-        self.X = X
+        self.data = X
+        self.centroids = sorted(X)
 
-        
+        while True:
+            new_centroids = []
+
+            for center in centroids:
+                within_radius = []
+
+                for point in data:
+                    if (np.linalg.norm(point-center) <= self.radius):
+                        within_radius.append(point)
+
+                new_centroid = np.average(within_radius)
+
+                new_centroids.append(new_centroid)
+
+            # Remove duplicates
+            new_centroids = sorted(list(set(new_centroids)))
+
+            # Compare new_centroids with old centroids
+            if (new_centroids == centroids):
+                break
+
+            centroids = new_centroids.copy()
