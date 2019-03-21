37f38acb9ba7128dd055860c6310051c69c5e869
Manohar Mukku <manoharmukku@gmail.com>
2019-03-21 14:18:39 +0530

    Initial code

---
+++ main.py
@@ -0,0 +1,6 @@
+"""
+Author: Manohar Mukku
+Date: 21 Mar 2019
+Desc: Mean shift clustering algorithm
+"""
+
---
+++ mean_shift.py
@@ -0,0 +1,14 @@
+"""
+Author: Manohar Mukku
+Date: 21 Mar 2019
+Desc: Mean shift clustering algorithm
+"""
+
+class MeanShift:
+    def __init__(self, radius=4):
+        self.radius = radius
+
+    def fit(X):
+        self.X = X
+
+        
