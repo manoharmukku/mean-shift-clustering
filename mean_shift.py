67d15339dfd887e17d7d095b91c58a2e5fba1f98
Manohar Mukku <manoharmukku@gmail.com>
2019-03-21 17:25:02 +0530

    Fixed bugs

--- mean_shift.py
+++ mean_shift.py
@@ -10,29 +10,44 @@ class MeanShift:
     def __init__(self, radius=1):
         self.radius = radius
 
-    def fit(X):
+    def fit(self, X):
         self.data = X
-        self.centroids = sorted(X)
+        self.centroids = X.copy()
+        self.centroids.sort(axis=0)
 
         while True:
+
+            # List which stores all the new centroids formed
             new_centroids = []
 
-            for center in centroids:
-                within_radius = []
+            # Iterate over all centroids individually
+            for centroid in self.centroids:
 
-                for point in data:
-                    if (np.linalg.norm(point-center) <= self.radius):
-                        within_radius.append(point)
+                # List which stores all the points within the radius of current 'centroid'
+                points_within_radius = []
 
-                new_centroid = np.average(within_radius)
+                # Find all the points within the radius of 'centroid'
+                for point in self.data:
 
-                new_centroids.append(new_centroid)
+                    if (np.linalg.norm(point-centroid) <= self.radius):
+                        points_within_radius.append(point)
+
+                if (len(points_within_radius) > 0):
+                    points_within_radius = np.array(points_within_radius)
+
+                    new_centroid = np.mean(points_within_radius, axis=0)
+
+                    new_centroids.append(new_centroid)
 
             # Remove duplicates
-            new_centroids = sorted(list(set(new_centroids)))
+            new_centroids = np.unique(new_centroids, axis=0)
 
-            # Compare new_centroids with old centroids
-            if (new_centroids == centroids):
+            # Sort new_centroids
+            new_centroids.sort(axis=0)
+
+            # If new_centroids is same as old centroids, saturation reached, stop
+            if (np.array_equal(self.centroids, new_centroids)):
                 break
 
-            centroids = new_centroids.copy()
+            # Else, update the old centroids with the new ones
+            self.centroids = new_centroids.copy()
